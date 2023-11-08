922a9941c39ad6e390777a4a489a58e911da9f7a
Sophia Horng <sh4230@columbia.edu>
2023-11-08 18:20:01 -0500

    project setup

---
+++ teamhungary/teamhungary/teamhungaryApp.swift
@@ -0,0 +1,17 @@
+//
+//  teamhungaryApp.swift
+//  teamhungary
+//
+//  Created by Sophia Horng on 11/8/23.
+//
+
+import SwiftUI
+
+@main
+struct teamhungaryApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
