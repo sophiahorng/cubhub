019168f0b63a5d24f96163cccd31ae67ecef7c4e
nicolemen <nicole.men.21@bishops.com>
2023-11-08 18:18:00 -0500

    initial project

---
+++ hungary/hungary/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  hungary
+//
+//  Created by Nicole Men on 11/8/23.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ hungary/hungary/hungaryApp.swift
@@ -0,0 +1,17 @@
+//
+//  hungaryApp.swift
+//  hungary
+//
+//  Created by Nicole Men on 11/8/23.
+//
+
+import SwiftUI
+
+@main
+struct hungaryApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
