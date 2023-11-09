71823644500b4b2aa3686129754a95586add85e7
Sophia Horng <sh4230@columbia.edu>
2023-11-09 11:38:46 -0500

    Added more views

---
+++ teamhungary/teamhungary/AddEventView.swift
@@ -0,0 +1,20 @@
+//
+//  AddEventView.swift
+//  teamhungary
+//
+//  Created by Sophia Horng on 11/9/23.
+//
+
+import SwiftUI
+
+struct AddEventView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+struct AddEventView_Previews: PreviewProvider {
+    static var previews: some View {
+        AddEventView()
+    }
+}
