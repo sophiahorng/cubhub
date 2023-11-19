7f17a7053f4517391f90c0030248da4873049931
Sophia Horng <sh4230@columbia.edu>
2023-11-18 16:56:50 -0800

    transferred event, events, addevent, and mapview pages from events branch (unable to merge due to conflicts)

--- teamhungary/teamhungary/AddEventView.swift
+++ teamhungary/teamhungary/AddEventView.swift
@@ -1,15 +1,74 @@
-//
+/
 //  AddEventView.swift
 //  teamhungary
 //
-//  Created by Sophia Horng on 11/18/23.
+//  Created by Sophia Horng on 11/9/23.
 //
 
 import SwiftUI
 
 struct AddEventView: View {
     var body: some View {
-        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+        VStack(spacing: 30) {
+            HStack{
+                Text("Add New Event")
+                    .font(.title)
+                    .fontWeight(.bold)
+                    .padding(20.0)
+                Spacer()
+
+                Button(action: {}) {
+                    Text("Done")
+                        .padding(20.0)
+                }
+            }
+
+            Image(systemName: "photo")
+                .resizable()
+                .frame(width: 170, height: 170)
+                .clipShape(Circle())
+                .overlay(Circle().stroke(Color.gray, lineWidth: 2))
+                .padding()
+
+            TextField("Event Name", text: .constant(""))
+                .textFieldStyle(RoundedBorderTextFieldStyle())
+                .frame(width: 300)
+                .multilineTextAlignment(.center)
+                .padding()
+                .background(Color(hue: 0.571, saturation: 1.0, brightness: 1.0, opacity: 0.541))
+                .foregroundColor(Color(hue: 0.571, saturation: 1.0, brightness: 1.0, opacity: 0.541))
+
+            TextField("Event Date", text: .constant(""))
+                .textFieldStyle(RoundedBorderTextFieldStyle())
+                .frame(width: 300)
+                .multilineTextAlignment(.center)
+                .padding()
+                .background(Color(hue: 0.571, saturation: 1.0, brightness: 1.0, opacity: 0.541))
+                .foregroundColor(Color(hue: 0.571, saturation: 1.0, brightness: 1.0, opacity: 0.541))
+
+            TextField("Event Location", text: .constant(""))
+                .textFieldStyle(RoundedBorderTextFieldStyle())
+                .frame(width: 300)
+                .multilineTextAlignment(.center)
+                .padding()
+                .background(Color(hue: 0.571, saturation: 1.0, brightness: 1.0, opacity: 0.541))
+                .foregroundColor(Color(hue: 0.571, saturation: 1.0, brightness: 1.0, opacity: 0.541))
+            
+            ScrollView{
+                TextField("Event Description", text: .constant(""))
+                    .textFieldStyle(RoundedBorderTextFieldStyle())
+                    .frame(width: 300)
+                    .multilineTextAlignment(.center)
+                    .padding()
+                    .background(Color(hue: 0.571, saturation: 1.0, brightness: 1.0, opacity: 0.541))
+                    .foregroundColor(Color(hue: 0.571, saturation: 1.0, brightness: 1.0, opacity: 0.541))
+                    
+                    
+            }
+
+            Spacer().frame(height: 13.0)
+            
+        }
     }
 }
 
@@ -18,3 +77,4 @@ struct AddEventView_Previews: PreviewProvider {
         AddEventView()
     }
 }
+
--- teamhungary/teamhungary/EventView.swift
+++ teamhungary/teamhungary/EventView.swift
@@ -2,15 +2,124 @@
 //  EventView.swift
 //  teamhungary
 //
-//  Created by Sophia Horng on 11/18/23.
+//  Created by Sophia Horng on 11/9/23.
 //
 
 import SwiftUI
-
+struct photo: Identifiable {
+    let id = UUID()
+    let name: String
+}
+struct user: Identifiable {
+    let id = UUID()
+    let name: String
+    let photo: String
+}
 struct EventView: View {
+    let images: [photo] = [photo(name: "photo"), photo(name: "photo"), photo(name: "photo"), photo(name: "photo")]
+    let users: [user] = [user(name: "Name", photo: "photo"), user(name: "Name", photo: "photo")]
     var body: some View {
-        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+        TabView {
+            VStack (spacing: 2) {
+                HStack {
+                    Button (action: {}) {
+                        HStack {
+                            Image(systemName: "chevron.backward")
+                            Text("Back")
+                        }.padding()
+                    }
+                    .frame(alignment: .leading)
+                    Text("Event 1")
+                        .font(.largeTitle)
+                        .fontWeight(.bold)
+                        .padding()
+                        .frame(maxWidth: .infinity, alignment: .center)
+
+                    Button(action: {
+                        
+                    }) {
+                        Image(systemName: "message").padding(20)
+                    }
+                    .frame(alignment: .trailing)
+                }
+                Spacer()
+                List {
+                    Section(header: Text("Event Photos")){
+                        HStack {
+                            ForEach(images) { image in
+                                Image(systemName: image.name)
+                                    .resizable()
+                                    .aspectRatio(contentMode: .fit)
+                                    .frame(width: 60, height: 60)
+                                    .clipShape(Rectangle())
+                                    .overlay(Rectangle().stroke(Color.gray, lineWidth: 2))
+                                    .padding(8)
+                            }
+                        }.padding(8)
+                        Button(action: {
+                            // Filter action
+                        }) {
+                            Text("Open Photo Gallery")
+                                .foregroundColor(.blue)
+                        }.padding(8)
+                    }
+                }
+                List {
+                    Section(header: Text("Attendees")) {
+                        VStack {
+                            ForEach(users) { user in
+                                HStack {
+                                    Image(systemName: user.photo)
+                                        .resizable()
+                                        .aspectRatio(contentMode: .fit)
+                                        .frame(width: 40, height: 40)
+                                        .clipShape(Circle())
+                                        .overlay(Circle().stroke(Color.gray, lineWidth: 2))
+                                        .padding(8)
+                                    Text(user.name)
+                                }
+                            }
+                        }
+                        Button(action: {
+                            // Filter action
+                        }) {
+                            Text("See More")
+                                .foregroundColor(.blue)
+                        }
+                        Button(action: {
+                            // Filter action
+                        }) {
+                            Text("Invite Someone")
+                                .foregroundColor(.blue)
+                        }
+                    }
+                }
+                Button(action: {
+                    
+                }) {
+                    Text("Add to Calendar")
+                }.padding()
+            }
+            .navigationBarHidden(true)
+            .tabItem {
+                Image(systemName: "list.bullet")
+                Text("Events")
+            }
+
+            Text("My Plans")
+                .tabItem {
+                    Image(systemName: "calendar")
+                    Text("My Plans")
+                }
+
+            Text("Account")
+                .tabItem {
+                    Image(systemName: "person")
+                    Text("Account")
+                }
+        }
     }
+    
 }
 
 struct EventView_Previews: PreviewProvider {
@@ -18,3 +127,4 @@ struct EventView_Previews: PreviewProvider {
         EventView()
     }
 }
+
