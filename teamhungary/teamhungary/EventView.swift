c2b86fbbb423749e748649a665230943b24c1044
Sophia Horng <sh4230@columbia.edu>
2023-11-18 16:53:32 -0800

    adding needed files

---
+++ teamhungary/teamhungary/EventView.swift
@@ -0,0 +1,20 @@
+//
+//  EventView.swift
+//  teamhungary
+//
+//  Created by Sophia Horng on 11/18/23.
+//
+
+import SwiftUI
+
+struct EventView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+struct EventView_Previews: PreviewProvider {
+    static var previews: some View {
+        EventView()
+    }
+}
