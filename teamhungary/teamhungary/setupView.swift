8c750933fc48ef50bd5f1c587833ba059b3c6960
nicolemen <nicole.men.21@bishops.com>
2023-12-01 21:45:16 -0500

    manually merged setup view into dev branch

---
+++ teamhungary/teamhungary/setupView.swift
@@ -0,0 +1,18 @@
+//
+//  setupView.swift
+//  teamhungary
+//
+//  Created by Nicole Men on 12/1/23.
+//
+
+import SwiftUI
+
+struct setupView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    setupView()
+}
