4404422f587e8ac0d3fee0bd6a06ce8a264b329a
Sophia Horng <sh4230@columbia.edu>
2023-12-01 19:50:54 -0500

    initial setup for SQLite

---
+++ teamhungary/teamhungary/SQLDatabaseManager.swift
@@ -0,0 +1,56 @@
+//
+//  SQLDatabaseManager.swift
+//  teamhungary
+//
+//  Created by Sophia Horng on 12/1/23.
+//
+
+import Foundation
+import SQLite
+
+class DatabaseManager {
+    static let shared = DatabaseManager()
+
+    private var db: Connection?
+
+    private init() {
+        do {
+            // Initialize the SQLite database connection
+            let path = try FileManager.default
+                .url(for: .documentDirectory, in: .userDomainMask, appropriateFor: nil, create: true)
+                .appendingPathComponent("your_database.sqlite3")
+                .path
+
+            db = try Connection(path)
+
+                // Call methods to create tables or perform any other setup
+            try createTables()
+        } catch {
+            print("Error initializing database: \(error)")
+        }
+    }
+
+    private func createTables() throws {
+        // Implement table creation logic here
+        let users = Table("users")
+        let id = Expression<Int>("id")
+        let firstname = Expression<String>("first_name")
+        let lastname = Expression<String>("last_name")
+        let email = Expression<String>("email")
+        let gradyear = Expression<String>("graduation_year")
+        let school = Expression<String>("school")
+        let iguser = Expression<String>("instagram_username")
+        let pfPicPath = Expression<String?>("profile_picture_path")
+
+        try db?.run(users.create { table in
+            table.column(id, primaryKey: .autoincrement)
+            table.column(firstname)
+            table.column(lastname)
+            table.column(email, unique: true)
+            table.column(gradyear, check: gradyear.like("20[2-9][0-9]"))
+            table.column(school)
+            table.column(iguser)
+            table.column(pfPicPath)
+        })
+    }
+}
