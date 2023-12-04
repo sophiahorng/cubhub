909969565bab68d1629a90b76dd22f8bd00e62d0
jasmine <yx2810@columbia.edu>
2023-12-04 17:19:44 -0500

    added bottom tab color

--- teamhungary/teamhungary/DefaultView.swift
+++ teamhungary/teamhungary/DefaultView.swift
@@ -13,30 +13,43 @@ struct DefaultView: View {
     @EnvironmentObject var loginState: LoginState
     var body: some View {
         TabView {
-            // EventsView
-            NavigationView {
-                EventsView(userData: $userData)
+            Group {
+                // EventsView
+                NavigationView {
+                    EventsView(userData: $userData)
+                }
+                .tabItem {
+                    Label("Events", systemImage: "list.bullet")
+                }
+                
+                
+                    NavigationView {
+                    MyPlansView(userData: $userData)
+                }
+                .tabItem {
+                    Label("My Plans", systemImage: "star")
+                }
+                
+                
+                    NavigationView {
+                    MyPageView(userData: $userData, loginState: _loginState)
+                }
+                .tabItem {
+                    Label("Profile", systemImage: "person")
+                }
             }
-            .tabItem {
-                Label("Events", systemImage: "list.bullet")
-            }
-            
-            
-            NavigationView {
-                MyPlansView(userData: $userData)
-            }
-            .tabItem {
-                Label("My Plans", systemImage: "star")
-            }
-            
-            
-            NavigationView {
-                MyPageView(userData: $userData, loginState: _loginState)
-            }
-            .tabItem {
-                Label("Profile", systemImage: "person")
-            }
+
+            .toolbarBackground(.visible, for: .tabBar)
+            .toolbarBackground(Color.white, for: .tabBar)
         }
     }
 }
+
+
+struct DefaultView_Previews: PreviewProvider {
+    static var previews: some View {
+        DefaultView(userData: UserData(url: nil, uid: "yx2810", name: "jasmine xin", email: "yx2810@columbia.edu"), isLogin: true)
+    }
+}
+
 
