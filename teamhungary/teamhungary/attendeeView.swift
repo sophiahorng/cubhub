8e3324614ed5976d5c9c247cbe6d98d3098585b8
nicolemen <nicole.men.21@bishops.com>
2023-12-05 13:38:06 -0500

    pushing attendee view again

--- teamhungary/teamhungary/attendeeView.swift
+++ teamhungary/teamhungary/attendeeView.swift
@@ -9,12 +9,25 @@
 
 import SwiftUI
 
-struct attendeeView: View {
-    var body: some View {
-        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
-    }
-}
+import FirebaseCore
+import FirebaseFirestore
 
-#Preview {
-    attendeeView()
+struct attendeeView: View {
+    
+    
+    @Binding var userData: UserData
+    var body: some View {
+        Spacer()
+        AsyncImage(url: userData.url)
+            .imageScale(.small)
+            .frame(width: 180, height: 180, alignment: .center)
+            .padding(EdgeInsets(top: 0, leading: 0, bottom: 0, trailing: 0))
+        Text(userData.name)
+        Text(userData.email)
+        Text("\(userData.school) \(userData.gradYear)")
+        if (!userData.igprof.isEmpty) {
+            Link("Instagram", destination: URL(string: "https://www.instagram.com/\(userData.igprof)")!)
+        }
+        Spacer()
+    }
 }
