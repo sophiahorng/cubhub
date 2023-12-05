09e79cf9c8dbd730d2b811dea4b93c11c46eb994
WenshuoXie/restaurant-microservice <wxie48@wisc.edu>
2023-12-05 09:35:51 -0500

    Finished search function (match strings), scroll view, the data is linked to the Firestore database rather than array, implemented real-time update.

--- teamhungary/teamhungary/EventsView.swift
+++ teamhungary/teamhungary/EventsView.swift
@@ -1,10 +1,6 @@
-//
-//  EventsView.swift
-//  teamhungary
-//
-//  Created by Sophia Horng on 11/9/23.
-//
 import SwiftUI
+import Firebase
+
 struct Event: Identifiable {
     var id: String = UUID().uuidString
     var eventName: String
@@ -15,144 +11,131 @@ struct Event: Identifiable {
     var eventLat: Double
     var eventOwner: String
     var attendees: [String]
-//    var eventDescription: String
+    // var eventDescription: String
 }
+
 struct EventsView: View {
-    @State private var events: [Event] = [
-        Event(eventName: "Jazz Night", eventDate: "10/31", eventAddress: "411 W 116th St, New York, NY 10027", eventLocation: "Columbia", eventLon: -73.9626, eventLat: 40.8075, eventOwner: "", attendees: []/*, eventDescription: "Jazz concert at Roone Arledge Auditorium featuring Christmas tunes"*/),
-        Event(eventName: "Broadway Show Preview", eventDate: "11/20", eventAddress: "411 W 116th St, New York, NY 10027", eventLocation: "Theatre", eventLon: -73.9855, eventLat: 40.7580, eventOwner: "", attendees: []/*, eventDescription: "Preview of Broadway show Phantom of the Opera"*/),
-        Event(eventName: "Art Walk", eventDate: "12/23", eventAddress: "411 W 116th St, New York, NY 10027", eventLocation: "Central Park", eventLon: -73.935242, eventLat: 40.730610, eventOwner: "", attendees: []/*, eventDescription: "Take a walk through Central Park to see  art pieces"*/)
-    ]
+    @State private var events: [Event] = []
     @State private var isAddEventViewPresented = false
+    @State private var searchText = ""
     @Binding var userData: UserData
     @State private var editMode: EditMode = .inactive
+
     var body: some View {
-//            TabView {
-                VStack (spacing: 0){
-                    HStack {
-                        Text("Events")
-                            .font(.title)
-                            .fontWeight(.bold)
-                            .padding()
-                        Spacer()
-                        Button(action: {
-                            isAddEventViewPresented.toggle()
-                        }) {
-                            Image(systemName: "plus")
-                                .padding()
-                        }
-                        .sheet(isPresented: $isAddEventViewPresented) {
-                            AddEventView(events: $events, userData: $userData, doneAction: {
-                                    isAddEventViewPresented.toggle()
-                                })
-                        }
-                    }
-                    HStack {
-                        Button(action: {
-                            // Filter
-                        }) {
-                            Text("Filter by...")
-                                .padding()
-                                .foregroundColor(.blue)
-                        }
-                        Spacer()
-                        // MapView
-                        NavigationStack {
-                            NavigationLink(destination: NewMapView(events: events)) {
-                            Text("See Map View")
-                                .padding()
-                                .foregroundColor(.blue)
-                            }
-                        } // NavigationStack end
-                    }
-                    
-                    NavigationStack{
-                        List {
-                            ForEach(events) { event in
-                                //NavigationView{
-                                NavigationLink(destination: EventView(event: event, userData: $userData)) {
-                                    HStack {
-//                                        Image(systemName: event.imageName)
-//                                            .resizable()
-//                                            .frame(width: 50, height: 50)
-//                                            .clipShape(Circle())
-//                                            .overlay(Circle().stroke(Color.gray, lineWidth: 2))
-//                                            .padding()
-                                        
-                                        VStack(alignment: .leading) {
-                                            Text(event.eventName + " on " + event.eventDate)
-                                                .font(.headline)
-                                            Text(event.eventLocation)
-                                                .font(.subheadline)
-                                                .foregroundColor(.black)
-//                                            Text(event.eventSubtitle)
-//                                                .font(.subheadline)
-//                                                .foregroundColor(.gray)
-                                        }
-                                        if editMode.isEditing {
-                                            Button(action: {
-                                                deleteEvent(event)
-                                            }) {
-                                                Image(systemName: "trash")
-                                                    .foregroundColor(.red)
-                                            }
-                                        }
-                                    }
-                                    //}
-                                }
-                            }
-                            .onDelete { indexSet in
-                                events.remove(atOffsets: indexSet)
-                            }
-                        }
-                        
-                        .environment(\.editMode, $editMode)  // Bind EditMode to the environment
-                        .animation(.default, value: editMode)
-                        .onAppear {
-                            UITableView.appearance().allowsSelectionDuringEditing = true
-                        }
-                        .background(Color("ColumbiaBlue"))
-                        .scrollContentBackground(.hidden)
-                        
-                    }
-                    
-                   
-                }
-//                .tabItem {
-//                    Image(systemName: "list.bullet")
-//                    Text("Events")
-//                }
-//
-//                Text("My Plans")
-//                    .tabItem {
-//                        Image(systemName: "calendar")
-//                        Text("My Plans")
-//                    }
-//
-//                Text("Account")
-//                    .tabItem {
-//                        Image(systemName: "person")
-//                        Text("Account")
-//                    }
-//            }
-//
-//            .background(
-//                Image("Background")
-//                    .resizable()
-//                    .scaledToFill()
-//                    .edgesIgnoringSafeArea(.all)
-//            )
+        NavigationView {
+            VStack(spacing: 0) {
+                headerSection
+                eventsListSection
+            }
+            .navigationBarTitle("Events", displayMode: .inline)
+            .onAppear {
+                fetchEvents()
+            }
         }
-    
-    func deleteEvent(_ event: Event) {
+    }
+
+    private var headerSection: some View {
+        VStack {
+            HStack {
+                TextField("Search events...", text: $searchText)
+                    .textFieldStyle(RoundedBorderTextFieldStyle())
+                    .padding()
+
+                Button("Search") {
+                    fetchEvents()
+                }
+                .padding(.trailing, 10)
+            }
+
+            HStack {
+                NavigationLink(destination: NewMapView(events: events)) {
+                    Text("See Map View")
+                        .padding()
+                        .foregroundColor(.blue)
+                }
+                Spacer()
+                Button(action: {
+                    isAddEventViewPresented.toggle()
+                }) {
+                    Image(systemName: "plus")
+                        .padding()
+                }
+                .sheet(isPresented: $isAddEventViewPresented, onDismiss: fetchEvents) {
+                    AddEventView(events: $events, userData: $userData) {
+                        isAddEventViewPresented = false
+                    }
+                }
+            }
+        }
+    }
+
+    private var eventsListSection: some View {
+        List {
+            ForEach(filteredEvents) { event in
+                NavigationLink(destination: EventView(event: event, userData: $userData)) {
+                    VStack(alignment: .leading) {
+                        Text(event.eventName)
+                            .font(.headline)
+                        Text("Date: \(event.eventDate) at \(event.eventLocation)")
+                            .font(.subheadline)
+                            .foregroundColor(.gray)
+                    }
+                }
+            }
+            .onDelete { indexSet in
+                events.remove(atOffsets: indexSet)
+            }
+        }
+        .listStyle(PlainListStyle())
+        .environment(\.editMode, $editMode)
+    }
+
+    private var filteredEvents: [Event] {
+        if searchText.isEmpty {
+            return events
+        } else {
+            return events.filter { event in
+                event.eventName.lowercased().contains(searchText.lowercased())
+            }
+        }
+    }
+
+    private func fetchEvents() {
+        let db = Firestore.firestore()
+        db.collection("events").getDocuments { (snapshot, error) in
+            if let error = error {
+                print("Error getting documents: \(error)")
+            } else {
+                if let snapshot = snapshot {
+                    self.events = snapshot.documents.compactMap { doc -> Event? in
+                        let data = doc.data()
+                        return Event(
+                            id: doc.documentID,
+                            eventName: data["name"] as? String ?? "",
+                            eventDate: data["date_time"] as? String ?? "",
+                            eventAddress: data["address"] as? String ?? "",
+                            eventLocation: data["location_name"] as? String ?? "",
+                            eventLon: data["lon"] as? Double ?? 0.0,
+                            eventLat: data["lat"] as? Double ?? 0.0,
+                            eventOwner: data["ownerID"] as? String ?? "",
+                            attendees: data["attendees"] as? [String] ?? []
+                        )
+                    }
+                }
+            }
+        }
+    }
+
+    private func deleteEvent(_ event: Event) {
         if let index = events.firstIndex(where: { $0.id == event.id }) {
             events.remove(at: index)
+            // Add logic to delete from Firebase
         }
     }
 }
 
 //struct EventsView_Previews: PreviewProvider {
 //    static var previews: some View {
-//        EventsView(userData: UserData(url: nil, uid: "u", name: "test", email: "test"))
+//        EventsView(userData: .constant(UserData(...)))
 //    }
 //}
