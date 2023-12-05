a5017629f4b019825ca9af364f842fd069dbc730
nicolemen <nicole.men.21@bishops.com>
2023-12-05 13:46:33 -0500

    very minor change to attendee view

--- teamhungary/teamhungary/attendeeView.swift
+++ teamhungary/teamhungary/attendeeView.swift
@@ -17,17 +17,20 @@ struct attendeeView: View {
     
     @Binding var userData: UserData
     var body: some View {
-        Spacer()
-        AsyncImage(url: userData.url)
-            .imageScale(.small)
-            .frame(width: 180, height: 180, alignment: .center)
-            .padding(EdgeInsets(top: 0, leading: 0, bottom: 0, trailing: 0))
-        Text(userData.name)
-        Text(userData.email)
-        Text("\(userData.school) \(userData.gradYear)")
-        if (!userData.igprof.isEmpty) {
-            Link("Instagram", destination: URL(string: "https://www.instagram.com/\(userData.igprof)")!)
+        VStack{
+            Spacer()
+            
+            AsyncImage(url: userData.url)
+                .imageScale(.small)
+                .frame(width: 180, height: 180, alignment: .center)
+                .padding(EdgeInsets(top: 0, leading: 0, bottom: 0, trailing: 0))
+            Text(userData.name)
+            Text(userData.email)
+            Text("\(userData.school) \(userData.gradYear)")
+            if (!userData.igprof.isEmpty) {
+                Link("Instagram", destination: URL(string: "https://www.instagram.com/\(userData.igprof)")!)
+            }
+            Spacer()
         }
-        Spacer()
     }
 }
